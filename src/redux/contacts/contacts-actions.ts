ddf48f81c90547f6bc5adda141d61cabc0846d78
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-08-15 04:21:54 +0800

    Update contacts-actions.ts

--- src/redux/contacts/contacts-actions.ts
+++ src/redux/contacts/contacts-actions.ts
@@ -3,15 +3,15 @@ import {IContact} from '../../interfaces/Contact.interface';
 
 const fetchContactsRequest = createAction('contacts/fetchContactsRequest');
 const fetchContactsSuccess = createAction<IContact[]>('contacts/fetchContactsSuccess');
-const fetchContactsError = createAction<string>('contacts/fetchContactsError');
+const fetchContactsError = createAction<string, 'contacts/fetchContactsError'>('contacts/fetchContactsError');
 
  const addContactRequest = createAction('contacts/addContactRequest');
- const addContactSuccess = createAction<IContact>('contacts/addContactSuccess');
- const addContactError = createAction<string>('contacts/addContactError');
+ const addContactSuccess = createAction<IContact, 'contacts/addContactSuccess'>('contacts/addContactSuccess');
+ const addContactError = createAction<string, 'contacts/addContactError'>('contacts/addContactError');
 
  const deleteContactRequest = createAction('contacts/deleteContactRequest');
- const deleteContactSuccess = createAction<string>('contacts/deleteContactSuccess');
- const deleteContactError = createAction<string>('contacts/deleteContactError');
+ const deleteContactSuccess = createAction<string, 'contacts/deleteContactSuccess'>('contacts/deleteContactSuccess');
+ const deleteContactError = createAction<string, 'contacts/deleteContactError'>('contacts/deleteContactError');
 
  const filterContact = createAction<string, 'contacts/filterContact'>('contacts/filterContact');
  // eslint-disable-next-line
